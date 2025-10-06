03a38a6a300a8ab4db1a9e43453e6505a9564092
jafethmontero <jafethmontero@gmail.com>
2025-10-06 16:53:40 -0600

    cambio de version

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello DevOps v1.0 🚀"));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => res.send("Hola DevOps v1.0 🚀"));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
